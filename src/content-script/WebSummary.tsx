b329f8ba2195f000f361b043092934ab09841137
givebest <givenlovs@msn.com>
2023-03-13 20:36:44 +0800

    feat:Web sumary stage3

--- src/content-script/WebSummary.tsx
+++ src/content-script/WebSummary.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'preact/hooks'
+import { useState, useCallback } from 'preact/hooks'
 import { Note, Description, Button, Divider, Card, Text, Link } from '@geist-ui/core'
 import { XCircleFillIcon, CopyIcon, ShareIcon } from '@primer/octicons-react'
 import Browser from 'webextension-polyfill'
+import ChatGPTQuery from '../content-script/ChatGPTQuery'
+import { extractFromHtml } from '@extractus/article-extractor'
+import { getUserConfig, Language, getProviderConfigs } from '../config'
+import { getSummaryPrompt } from '../content-script/prompt'
 import logoWhite from '../logo-white.png'
 import logo from '../logo.png'
 
 function WebSummary(props) {
   const [showCard, setShowCard] = useState(false)
+  const [question, setQuestion] = useState('')
 
   const onBack = () => {
     Browser.runtime.sendMessage({
@@ -19,6 +24,34 @@ function WebSummary(props) {
       return !state
     })
   }
+
+  const onSummary = useCallback(async () => {
+    const html = document.querySelector('html')?.outerHTML
+    const url = location.href
+
+    if (!html) {
+      return
+    }
+
+    const article = await extractFromHtml(html, url)
+    console.log('article', article)
+
+    if (article.content) {
+      const language = window.navigator.language
+      const userConfig = await getUserConfig()
+      const providerConfigs = await getProviderConfigs()
+
+      setQuestion(`Content:  ${getSummaryPrompt(
+        article.content.replace(/<[^>]+>/g, ''),
+        providerConfigs.provider,
+      )}
+
+Instructions: Please use the contents to summarize the highlights.
+
+Please write in ${userConfig.language === Language.Auto ? language : userConfig.language} language.
+      `)
+    }
+  }, [])
 
   return (
     <>
@@ -43,34 +76,31 @@ function WebSummary(props) {
             <Divider my={0} />
 
             <Card.Content className="glarity--summary__card--content">
-              Glarity - Google / YouTube 摘要扩展 Glarity 是一个谷歌 / YouTube 摘要扩展，它使用
-              ChatGPT 提供了搜索结果和视频的摘要，并支持
-              Yahoo!ニュース、PubMed、PMC、NewsPicks、Github、Nikkei、Bing 和
-              Google专利搜索。此外，它还支持官方的 OpenAI API 和 ChatGPT Plus。扩展还提供了 Markdown
-              渲染、代码高亮、暗模式、复制到剪贴板和切换语言等功能。您可以从 Chrome 网上应用店或
-              Mozilla 应用商店安装该扩展。如果您遇到问题，请参阅故障排除指南。此项目是
-              wong2/chatgpt-google-extension 的分支，借鉴了 qunash/chatgpt-advanced 和 YouTube
-              Summary with ChatGPT 的代码。此项目使用 GPL-3.0 许可证。
+              {question ? (
+                <div className="glarity--container">
+                  <div className="glarity--chatgpt">
+                    <ChatGPTQuery question={question} />
+                  </div>
+                </div>
+              ) : (
+                <Button type="success" scale={0.5} onClick={onSummary}>
+                  Summarize
+                </Button>
+              )}
             </Card.Content>
 
-            <Card.Footer className="glarity--justify-end">
+            {/* <Card.Footer className="glarity--justify-end">
               <Button scale={0.25} type="success" ghost icon={<CopyIcon />} auto>
                 Copy
               </Button>
-              {/* <Button scale={0.25} type="secondary" ghost icon={<ShareIcon />} auto>
+              <Button scale={0.25} type="secondary" ghost icon={<ShareIcon />} auto>
                 Share
-              </Button> */}
-            </Card.Footer>
+              </Button>
+            </Card.Footer> */}
           </Card>
         </div>
       ) : (
-        <Button
-          className={(styles('styles'), 'glarity--summary__btn')}
-          type="success"
-          auto
-          inline
-          onClick={onSwitch}
-        >
+        <Button className={'glarity--summary__btn'} type="success" auto inline onClick={onSwitch}>
           <img src={logoWhite} className="glarity--w-5 glarity--h-5 glarity--rounded-sm" />
         </Button>
       )}
