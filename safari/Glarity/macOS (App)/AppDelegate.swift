f45c73c198cbe98eae9fafc7dccf26a2f077ec14
givebest <givenlovs@msn.com>
2023-03-16 18:17:09 +0800

    feat:Support safari

---
+++ safari/Glarity/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Pell Wong on 2023/3/16.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
